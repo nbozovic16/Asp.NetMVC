a4dde35d5e87f5450f980a603f0ce26222bd36e8
nbozovic16 <nbozovic16@raf.rs>
2019-12-03 14:43:35 +0100

    INSERT new product

--- Project/Project/Controllers/ProductController.cs
+++ Project/Project/Controllers/ProductController.cs
@@ -5,54 +5,66 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.SqlClient;
 using System.Data;
+using Project.Models;
 
 namespace Project.Controllers
 {
     public class ProductController : Controller
     {
         string connectionString = @"Data Source=.;Initial Catalog=Asp.NetMVC;Integrated Security=True"; //connectio to DB
+        
 
 
-        // GET: Product
+        // GET: Product -> this route get all products from DB and pass it to the view
         [HttpGet]
         public ActionResult Index()
         {
             DataTable dtblProdcut = new DataTable();
-            using (SqlConnection sqlConn = new SqlConnection(connectionString)) //by what connection 
+            using (SqlConnection sqlConn = new SqlConnection(connectionString)) //by which connection 
             {
                 sqlConn.Open(); //open connection 
+
                 SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Product", sqlConn); //query for select all products
                 sqlDa.Fill(dtblProdcut); //filling object with data
+
+                sqlConn.Close(); //close connection
             }
                 return View(dtblProdcut); //passing data to View 
         }
 
-        // GET: Product/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+       
 
-        // GET: Product/Create
+        // GET: Product/Create -> this route get the form for insert new prodcut
+        [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            return View(new ProductModel());
         }
 
-        // POST: Product/Create
+        // POST: Product/Create -> this route post data to DB
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(ProductModel product)
         {
-            try
+            using (SqlConnection sqlConn = new SqlConnection(connectionString)) //by which connection
             {
-                // TODO: Add insert logic here
+                sqlConn.Open(); //open connection
 
-                return RedirectToAction("Index");
+                string query = "INSERT INTO Product VALUES(@Name, @Description, @Category, @Manufacturer, @Supplier, @Price)"; //query for insert new product
+
+                SqlCommand sqlCmd = new SqlCommand(query, sqlConn); //handling query over connection
+                sqlCmd.Parameters.AddWithValue("@Name", product.Name); //adding values
+                sqlCmd.Parameters.AddWithValue("@Description", product.Description);
+                sqlCmd.Parameters.AddWithValue("@Category", product.Category);
+                sqlCmd.Parameters.AddWithValue("@Manufacturer", product.Manufacturer);
+                sqlCmd.Parameters.AddWithValue("@Supplier", product.Supplier);
+                sqlCmd.Parameters.AddWithValue("@Price", product.Price);
+
+                sqlCmd.ExecuteNonQuery(); //execute query
+
+                sqlConn.Close(); //close connection
+
             }
-            catch
-            {
-                return View();
-            }
+                return RedirectToAction("Index"); //redirection 
         }
 
         // GET: Product/Edit/5
